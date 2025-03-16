1905cf82cf03a966e5b675955b4fecefb7a07a60
Joshua Key <keyjoshj98@yahoo.com>
2025-03-16 01:23:08 -0500

    Adding Camera Scene Object

---
+++ ComputerGraphics/Camera.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "SceneObject.h"
+
+/// <summary>
+/// Camera is responsible for determining the perspective of other objects in a Scene
+/// </summary>
+class Camera : public SceneObject
+{
+public:
+	Camera() { }
+};
+
